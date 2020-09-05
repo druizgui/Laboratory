9f8c2cb3a5d00327402d9852017b55c393150cef
David Ruiz <druizg@gmail.com>
2020-09-05 17:45:57 +0200

    Serilog console demo

---
+++ Serilog/SerilogConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace SerilogConsole
+{
+    using System;
+    using System.Threading;
+    using Microsoft.Extensions.Configuration;
+    using Serilog;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            Log.Logger.Information("Hello word with Serilog console app");
+
+            for (var i = 0; i < 10; i++)
+            {
+                Log.Logger.Verbose("Verbose " + i);
+                Log.Logger.Debug("Debug " + i);
+                Log.Logger.Information("Information " + i);
+                Log.Logger.Warning("Warning " + i);
+                Log.Logger.Error("Error " + i);
+                Log.Logger.Fatal("Fatal " + i);
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
---
+++ Serilog/SerilogConsole/SerilogExtensions.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------
+// <copyright>Copyright (C) 2020, David Ruiz.</copyright>
+// Licensed under the Apache License, Version 2.0.
+// You may not use this file except in compliance with the License:
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Software is distributed on an "AS IS", WITHOUT WARRANTIES
+// OR CONDITIONS OF ANY KIND, either express or implied.
+// -----------------------------------------------------------------
+
+namespace SerilogConsole
+{
+    using System;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Serilog;
+
+    public static class SerilogExtensions
+    {
+        public static IServiceCollection UseSerilog<T>(this IServiceCollection services, IConfigurationRoot configuration)
+        {
+            var type = typeof(T);
+            return InternalUseSerilog(services, configuration, type);
+        }
+
+        public static IServiceCollection UseSerilog(this IServiceCollection services, IConfigurationRoot configuration)
+        {
+            return InternalUseSerilog(services, configuration);
+        }
+
+        private static IServiceCollection InternalUseSerilog(IServiceCollection services, IConfigurationRoot configuration, Type typeContext = null)
+        {
+            var config = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration);
+            Log.Logger = config
+                .CreateLogger();
+
+            if (typeContext != null) Log.Logger.ForContext(typeContext);
+
+            AppDomain.CurrentDomain.ProcessExit += (s, e) => Log.CloseAndFlush();
+            return services.AddSingleton(Log.Logger);
+        }
+    }
+}
