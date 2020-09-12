a8a29d78e9a7a01d2e6d313198f3a05c0bb3d3ad
David Ruiz <druizg@gmail.com>
2020-09-12 14:07:42 +0200

    Responsive menu demo

---
+++ Html/ResponsiveMenu.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ResponsiveMenu.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
